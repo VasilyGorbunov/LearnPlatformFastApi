d46742a2d83559fbfa3c07ea76ed507784140d29
VasilyGorbunov <vasily.gorbunov@gmail.com>
2024-03-16 19:25:00 +0300

    3 - 14:46

--- api/handlers.py
+++ api/handlers.py
@@ -1,4 +1,7 @@
-from fastapi import APIRouter, Depends
+from typing import Union
+from uuid import UUID
+
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from api.models import UserCreate, ShowUser
@@ -27,6 +30,32 @@ async def _create_new_user(body: UserCreate, db) -> ShowUser:
             )
 
 
+async def _delete_user(user_id, db) -> Union[UUID, None]:
+    async with db as session:
+        async with session.begin():
+            user_dal = UserDAL(session)
+            deleted_user_id = await user_dal.delete_user(user_id=user_id)
+
+            return deleted_user_id
+
+
+async def _update_user(body, db) -> Union[UUID, None]:
+    async with db as session:
+        async with session.begin():
+            user_dal = UserDAL(session)
+            updated_user_id = await user_dal.update_user(**body.dict())
+
+            return updated_user_id
+
+
 @user_router.post("/", response_model=ShowUser)
 async def create_user(body: UserCreate, db: AsyncSession = Depends(get_db)) -> ShowUser:
     return await _create_new_user(body, db)
+
+@user_router.delete("/", response_model=DeleteUserResponse)
+async def delete_user(user_id: UUID, db: AsyncSession = Depends(get_db)) -> DeleteUserResponse:
+    deleted_user_id = _delete_user(user_id, db)
+    if deleted_user_id is None:
+        raise HTTPException(status_code=404, detail=f"User with id {user_id} not found")
+
+    return DeleteUserResponse(deleted_user_id=deleted_user_id)
